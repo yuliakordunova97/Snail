ee7acba61df70ac9328e581c3d4304e760c4e53a
yuliakordunova97 <kordunovayulia@gmail.com>
2022-03-16 10:17:53 +0200

    added files

---
+++ src/main/java/com/epam/rd/autotasks/snail/Snail.java
@@ -0,0 +1,11 @@
+package com.epam.rd.autotasks.snail;
+
+import java.util.Scanner;
+
+public class Snail {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
